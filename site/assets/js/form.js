e01782eb178b8833c8939a95ed79e063804a1122
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2025-04-06 20:57:45 +1000

    form changes

--- site/assets/js/form.js
+++ site/assets/js/form.js
@@ -11,57 +11,87 @@ const handleFormSubmission = (formId) => {
     const form = document.getElementById(formId);
     if (!form) return;
 
-        form.addEventListener('submit', async (e) => {
+    form.addEventListener('submit', async (e) => {
+
+        try {
+
             e.preventDefault();
+
+            // Get the elements
             const submitButton = form.querySelector('button[type="submit"]');
-        const errorElement = form.querySelector('#error');
-        const successElement = form.querySelector('#success');
-        const setDisplayState = (element, state) => element.style.display = state ? 'block' : 'none';
-        const toggleElements = (state) => {
-            setDisplayState(successElement, state === 'success');
-            setDisplayState(errorElement, state === 'error');
-        };
-        const resetSubmitButton = () => {
-            submitButton.classList.remove('loading');
-            submitButton.disabled = false;
-        }
+            const errorElement = form.querySelector('#error');
+            const successElement = form.querySelector('#success');
 
-        toggleElements('none');
-        submitButton.classList.add('loading');
-        submitButton.disabled = true;
+            const setDisplayState = (element, state) => {
+                if (element) element.style.display = state ? 'block' : 'none';
+            };
 
-        const formData = new FormData(form);
+            // Toggle the elements
+            const toggleElements = (state) => {
+                setDisplayState(successElement, state === 'success');
+                setDisplayState(errorElement, state === 'error');
+            };
 
-        if (typeof window !== 'undefined') {
-            formData.set('page', window.location.pathname);
-        }
-        formData.set('submission_time', new Date().toISOString());
+            // Reset the submit button
+            const resetSubmitButton = () => {
+                if (submitButton) {
+                    submitButton.classList.remove('loading');
+                    submitButton.disabled = false;
+                }
+            }
 
-        const token = turnstile.getResponse();
-        if (!token) {
+            toggleElements('none');
+
+            if (submitButton) {
+                submitButton.classList.add('loading');
+                submitButton.disabled = true;
+            }
+
+            const formData = new FormData(form);
+
+            if (typeof window !== 'undefined') {
+                formData.set('page', window.location.pathname);
+            }
+            formData.set('submission_time', new Date().toISOString());
+
+            // Get the token
+            let token = window.turnstile.getResponse();
+            if (!token) {
+                resetSubmitButton();
+                alert('Please complete the Turnstile challenge');
+                return;
+            }
+
+            formData.append('turnstileToken', token);
+
+            // Submit the form
+            const response = await safeFetch('https://forms-7fa3jw7jhq-uc.a.run.app/api/submit-form', {
+                method: 'POST',
+                body: formData,
+                credentials: 'include'
+            });
+
+            if (response?.ok) {
+                form.reset();
+                window.turnstile.reset();
+                toggleElements('success');
+            } else {
+                toggleElements('error');
+            }
             resetSubmitButton();
-            alert('Please complete the Turnstile challenge');
+        } catch (error) {
+            console.error('Turnstile error:', error);
+            resetSubmitButton();
+            toggleElements('error');
             return;
         }
-        formData.append('turnstileToken', token);
-
-        const response = await safeFetch('https://forms-7fa3jw7jhq-uc.a.run.app/api/submit-form', {
-            method: 'POST',
-            body: formData,
-            credentials: 'include'
-        });
-
-        if (response?.ok) {
-            form.reset();
-            turnstile.reset();
-            toggleElements('success');
-        } else {
-            toggleElements('error');
-        }
-        resetSubmitButton();
     });
 };
 
 document.addEventListener('DOMContentLoaded', function () {
+    // Handle contact form
     handleFormSubmission('contactForm');
+
+    // Handle newsletter forms
+    handleFormSubmission('newsletterForm');
 });
